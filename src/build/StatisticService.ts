7f4f18b98e80236584beead2f688007cf91a1e27
cjiping <cjiping@linewell.com>
2021-10-13 10:03:50 +0800

    新增代码频率统计

--- src/build/StatisticService.ts
+++ src/build/StatisticService.ts
@@ -10,10 +10,11 @@ class StatisticService extends BaseService {
   public static async generateStatistic(): Promise<StatisticInfo> {
     const info = new StatisticInfo()
     const filelist = this.listAllFile('doc')
-    const result = await Promise.all([this.getRepositorySize(), this.getCommitStatistic(), this.getWordStatistic()])
+    const result = await Promise.all([this.getRepositorySize(), this.getCommitStatistic(), this.getWordStatistic(), this.getCodeFrequency()])
     info.repositorySize = result[0]
     info.commit = result[1]
     info.word = result[2]
+    info.codeFrequency = result[3]
     info.chapterCount = filelist.filter(v => v.endsWith('.md') || v.endsWith('.MD')).length
     info.imageCount = filelist.filter(v => {
       for(let item of imageSuffix) {
@@ -63,6 +64,48 @@ class StatisticService extends BaseService {
       total: totalWords,
       wordPerDay: Math.ceil(totalWords / pastDays)
     }
+  }
+
+  private static async getCodeFrequency(): Promise<CodeFrequencyItem[]> {
+    const filelist = this.listAllMdFile()
+    const taskList: Promise<Buffer>[] = []
+    for(let i of filelist) {
+      if (i.startsWith('doc') || i.startsWith('./doc')) {
+        taskList.push(fs.promises.readFile(i))
+      }
+    }
+    const langMap = new Map<string, number>();
+
+    (await Promise.all(taskList))
+      .map(v => v.toString())
+      .map(v => v.split('\n'))
+      .flatMap(v => v)
+      .filter(v => v.indexOf('```') != -1)
+      .map(v => {
+        if (v.indexOf('```') != -1) {
+          return v.replace(/`/g, '')
+        }
+        return ''
+      })
+      .map(v => v.trim())
+      .filter(v => v)
+      .filter(v => v.length < 12)
+      .forEach(v => {
+        if (langMap.has(v)) {
+          langMap.set(v, langMap.get(v)! + 1)
+        }else{
+          langMap.set(v, 1)
+        }
+      })
+      return Array.from(langMap)
+                  .sort((a,b) => a[1] - b[1])
+                  .reverse()
+                  .map(v => {
+                    return {
+                      lang: v[0],
+                      frequency: v[1]
+                    } as CodeFrequencyItem
+                  })
   }
 
   static cleanText(text: string): string {
