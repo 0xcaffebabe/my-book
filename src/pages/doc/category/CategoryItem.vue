798270c99b1123b320ed03a28f505ed043fa2408
cjiping <cjiping@linewell.com>
2021-10-13 15:21:25 +0800

    目录项概览功能

--- src/pages/doc/category/CategoryItem.vue
+++ src/pages/doc/category/CategoryItem.vue
@@ -1,43 +1,87 @@
 <template>
   <el-popover
-        placement="right-start"
-        :title="value.name"
-        :width="200"
-        trigger="hover"
-        :hide-after="100"
-        content="this is content, this is content, this is content"
-      >
-        <template #reference>
-          <el-menu-item :index="convert(value.link)">
-            <template #title>
-              <span>{{ value.name }}</span>
-            </template>
-          </el-menu-item>
+    placement="right-start"
+    :width="200"
+    trigger="hover"
+    :hide-after="100"
+    @show="handlePopoverShow"
+    @hide="handlePopoverHide"
+    content="this is content, this is content, this is content"
+  >
+    <template #reference>
+      <el-menu-item :index="convert(value.link)">
+        <template #title>
+          <span>{{ value.name }}</span>
         </template>
-      </el-popover>
+      </el-menu-item>
+    </template>
+    <div>
+      <el-skeleton :rows="0" animated :loading="loading" :throttle="500">
+        <template #default>
+          <el-badge
+            :value="lastPastDays + '天前更新'"
+            class="item"
+            type="warning"
+            style="margin-left: 8px"
+          >
+            <span>{{ value.name }}</span>
+          </el-badge>
+        </template>
+      </el-skeleton>
+    </div>
+  </el-popover>
 </template>
 
 <script lang="ts">
-import Category from '@/dto/Category'
-import { defineComponent } from 'vue'
-import categoryService from '@/service/CategoryService'
+import Category from "@/dto/Category";
+import { defineComponent } from "vue";
+import categoryService from "@/service/CategoryService";
+import api from "@/api";
+import DocFileInfo from "@/dto/DocFileInfo";
 
 export default defineComponent({
   props: {
-    value: Category
+    value: Category,
   },
-  setup() {
-    
+  data() {
+    return {
+      loading: true as boolean,
+      file: null as DocFileInfo | null,
+      // 离最后一次更新过去几天
+      lastPastDays: 0 as number,
+    };
   },
+  setup() {},
   methods: {
     // 将doc链接转为 x-x-x 形式的id
     convert(link: string): string {
       return categoryService.docUrl2Id(link);
     },
-  }
-})
+    async handlePopoverShow() {
+      this.file = await api.getDocFileInfo(this.convert(this.value!.link));
+      this.lastPastDays = this.calcLastUpdate();
+      console.log(this.file);
+      this.loading = false;
+    },
+    handlePopoverHide() {
+      this.loading = true;
+    },
+    calcLastUpdate() {
+      if (!this.file) {
+        return -1;
+      }
+      const lastUpdateDate = this.file.commitList
+        .map((v) => v.date)
+        .map(Date.parse)
+        .sort((a, b) => a - b)
+        .reverse()[0];
+      return Math.ceil(
+        (new Date().getTime() - lastUpdateDate) / (3600 * 24 * 1000)
+      );
+    },
+  },
+});
 </script>
 
 <style lang="less" scoped>
-
 </style>
