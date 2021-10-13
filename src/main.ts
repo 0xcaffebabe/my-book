c4a43e9eac62cd1d2372dee1f5fc1cb067fb1901
cjiping <cjiping@linewell.com>
2021-10-13 15:37:44 +0800

    屏幕顶端进度条

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,36 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style.css'
 import './code-hl-vsc.css'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import axios from 'axios'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
-app.use(createRouter())
+const router = createRouter()
+
+// 屏幕顶端进度条开始
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+router.afterEach(() => {
+  NProgress.done()
+})
+
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+}, error => {
+  NProgress.done()
+  return Promise.resolve(error)
+})
+// 屏幕顶端进度条结束
+
+app.use(router)
 app.mount('#app')
